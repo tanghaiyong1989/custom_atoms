acae14d4e3e9445386b020fbca9e4916c6b0013c
HaiYong Tang <807808055@qq.com>
2019-08-14 17:36:59 +0800

    update 4

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -13,7 +13,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': 'class_db_prod',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
-        'PASSWORD': '123456',                        # 你的数据库password
+        'PASSWORD': 'L*vIW=.lTr',                        # 你的数据库password
         'HOST': '172.16.127.33',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
